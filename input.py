09c765a5710bb32af73c31003b0f349f1860dd81
cevahir-koprulu <cevahir.koprulu@hotmail.com>
2021-10-28 23:01:56 -0500

    input.py creates a DGL graph with health label main.py trains a DGL graph with GCN

---
+++ input.py
@@ -0,0 +1,111 @@
+import networkx as nx
+import random as rng
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+import scipy.stats as stat
+from enum import Enum
+import dgl
+
+r0 = 16 # Basic reproduction number (16 is typical for measles)
+INFECTIOUS_PERIOD = 8 # (8 is typical for measles)
+NUM_INFECTED = 100 # Initial infected population size
+NUM_DAYS = 31
+
+months = [1]
+sir = {'S': set(),
+       'I': (set(), {}),
+       'R': set()}
+S = []
+I = []
+R = []
+
+FEAT_AGE = {"NaN": [0 for i in range(14)]}
+for i in range(20,90,5):
+    key = "Age_"+str(i)+"_to_"+str(i+4)
+    feat = [0 for i in range(14)]
+    idx = i//5-4
+    feat[idx] = 1
+    FEAT_AGE[key] = feat
+
+print("GENERATING SUSCEPTIBLE POPULATION...")
+for month in tqdm(months, position=0, desc="months", leave=False, colour='green', ncols=80):
+    for day in tqdm(range(1, NUM_DAYS+1), position=1, desc="days  ", leave=False, colour='red', ncols=80):
+        try:
+            filename = 'Data/2020/{:02d}/2020-{:02d}-{:02d}.gexf'.format(month, month, day)
+            G = nx.read_gexf(filename)
+        except:
+            continue
+        sir['S'].update(G.nodes)
+
+pop_size = len(sir['S'])
+
+print("SIMULATING DISEASE SPREAD...")
+first_day = True
+
+for month in tqdm(months, position=0, desc="months", leave=False, colour='green', ncols=80):
+    for day in tqdm(range(1, NUM_DAYS+1), position=1, desc="days", leave=False, colour='red', ncols=80):
+        try:
+            filename = 'Data/2020/{:02d}/2020-{:02d}-{:02d}.gexf'.format(month, month, day)
+            G = nx.read_gexf(filename)
+        except:
+            continue
+
+        if first_day:
+            #Compute spreadiong constant
+            degrees = np.zeros(len(G.nodes))
+            idx = 0
+            for i in G.nodes:
+                for j in nx.neighbors(G, i):
+                    degrees[idx] += G[i][j]['time_spent']
+                idx += 1
+
+            rho = r0 / (INFECTIOUS_PERIOD * np.mean(degrees))
+
+            infected_init = rng.sample(G.nodes, NUM_INFECTED)
+            for i in infected_init:
+                sir['S'].remove(i)
+                sir['I'][0].add(i)
+                sir['I'][1][i] = 0
+            first_day = False
+            
+        temp = sir['I'][0].copy()
+        for i in temp:
+            sir['I'][1][i] += 1
+            day = sir['I'][1][i]
+            if day >= INFECTIOUS_PERIOD:
+                sir['I'][0].remove(i)
+                sir['I'][1].pop(i)
+                sir['R'].add(i)
+                continue
+            if i in G.nodes:
+                for j in nx.neighbors(G, i):
+                    if j in sir['S']:
+                        D = G[i][j]['time_spent']
+                        if rng.random() < 1 - (1-rho)**D:
+                            sir['S'].remove(j)
+                            sir['I'][0].add(j)
+                            sir['I'][1][j] = 0
+
+filename = 'Data/2020/{:02d}/2020-{:02d}-{:02d}.gexf'.format(months[0], months[0], NUM_DAYS)
+G = nx.read_gexf(filename)
+attrs = {}
+ages = nx.get_node_attributes(G,'age')
+for n in G.nodes:
+    feat_age = FEAT_AGE[ages[n]]
+    if n in sir['S']:
+        attrs[n] = {"health": 0,"age":feat_age}
+    elif n in sir['I'][0]:
+        attrs[n] = {"health": 1,"age":feat_age}    
+    elif n in sir['R']:
+        attrs[n] = {"health": 2,"age":feat_age}
+    else:
+        raise ValueError("WHERE IS THIS NODE??")
+        
+nx.set_node_attributes(G, attrs)
+G = G.to_directed()
+G_dgl = dgl.from_networkx(G, node_attrs=['health','age'], edge_attrs=['time_spent'])
+filename = '2020-{:02d}-{:02d}.dgl'.format(months[0], NUM_DAYS)
+dgl.save_graphs(filename, G_dgl)
+
+
--- main.py
+++ main.py
@@ -1,16 +1,97 @@
-# This is a sample Python script.
+import dgl
+from dgl.nn import GraphConv
+from dgl.data import DGLDataset
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class Dataset(DGLDataset):
+    def __init__(self, name, filename):
+        self.filename = filename
+        super().__init__(name=name)
+        
+    def process(self):
+        (self.graph,), _ = dgl.load_graphs(self.filename)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        # If your dataset is a node classification dataset, you will need to assign
+        # masks indicating whether a node belongs to training, validation, and test set.
+        n_nodes = self.graph.num_nodes()
+        n_train = int(n_nodes * 0.6)
+        n_val = int(n_nodes * 0.2)
+        train_mask = torch.zeros(n_nodes, dtype=torch.bool)
+        val_mask = torch.zeros(n_nodes, dtype=torch.bool)
+        test_mask = torch.zeros(n_nodes, dtype=torch.bool)
+        train_mask[:n_train] = True
+        val_mask[n_train:n_train + n_val] = True
+        test_mask[n_train + n_val:] = True
+        self.graph.ndata['train_mask'] = train_mask
+        self.graph.ndata['val_mask'] = val_mask
+        self.graph.ndata['test_mask'] = test_mask
 
+    def __getitem__(self, i):
+        return self.graph
 
-# Press the green button in the gutter to run the script.
+    def __len__(self):
+        return 1
+
+class GCN(nn.Module):
+    def __init__(self, in_feats, h_feats, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GraphConv(in_feats, h_feats)
+        self.conv2 = GraphConv(h_feats, num_classes)
+
+    def forward(self, g, in_feat):
+        h = self.conv1(g, in_feat)
+        h = F.relu(h)
+        h = self.conv2(g, h)
+        return h
+
+def train(g, model):
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    best_val_acc = 0
+    best_test_acc = 0
+
+    features = g.ndata['age']
+    labels = g.ndata['health']
+    train_mask = g.ndata['train_mask']
+    val_mask = g.ndata['val_mask']
+    test_mask = g.ndata['test_mask']
+    for e in range(100):
+        # Forward
+        logits = model(g, features)
+
+        # Compute prediction
+        pred = logits.argmax(1)
+        # Compute loss
+        # Note that you should only compute the losses of the nodes in the training set.
+        loss = F.cross_entropy(logits[train_mask], labels[train_mask])
+
+        # Compute accuracy on training/validation/test
+        train_acc = (pred[train_mask] == labels[train_mask]).float().mean()
+        val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
+        test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
+
+        # Save the best validation accuracy and the corresponding test accuracy.
+        if best_val_acc < val_acc:
+            best_val_acc = val_acc
+            best_test_acc = test_acc
+
+        # Backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if e % 5 == 0:
+            print('In epoch {}, loss: {:.3f}, val acc: {:.3f} (best {:.3f}), test acc: {:.3f} (best {:.3f})'.format(
+                e, loss, val_acc, best_val_acc, test_acc, best_test_acc))
+
+def main():
+    filename = '2020-01-31.dgl'
+    name = "SIR"
+    dataset = Dataset(name=name,filename=filename)
+    graph = dataset[0]
+    model = GCN(graph.ndata['age'].shape[1], 16, 3)
+    train(graph, model)
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
